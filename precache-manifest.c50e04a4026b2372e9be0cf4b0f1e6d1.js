254f1c3dc2dee09b855708113687a0af87a2f6e3
Ssuper-big-mushroom <18816237025@163.com>
2019-12-28 16:14:37 +0800

    修改框架

---
+++ precache-manifest.c50e04a4026b2372e9be0cf4b0f1e6d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4810d71d38d8156100be89ab9f733e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34487f0fbe0f308ef648",
+    "url": "/static/css/2.b91be635.chunk.css"
+  },
+  {
+    "revision": "602d0e1a77a3ab9775b0",
+    "url": "/static/css/main.b65f9536.chunk.css"
+  },
+  {
+    "revision": "34487f0fbe0f308ef648",
+    "url": "/static/js/2.f058f322.chunk.js"
+  },
+  {
+    "revision": "77fbbfd49f42ad69bafec34d9679718a",
+    "url": "/static/js/2.f058f322.chunk.js.LICENSE"
+  },
+  {
+    "revision": "602d0e1a77a3ab9775b0",
+    "url": "/static/js/main.590ab3b8.chunk.js"
+  },
+  {
+    "revision": "3784fcfe889dc9bb639d",
+    "url": "/static/js/runtime-main.82879bc6.js"
+  }
+]);
